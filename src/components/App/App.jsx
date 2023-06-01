26ecac7b329add409273b33af7c575ca261b8dd1
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-06-01 21:40:20 +0200

    333

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import { AppContainer } from './App.styled';
+import { GlobalStyle } from '../GlobalStyle';
+import ContactForm from 'components/ContactForm/ContactForm';
+import Filter from 'components/Filter/Filter';
+import ContactList from 'components/ContactList/ContactList';
+
+export const App = () => {
+  return (
+    <AppContainer>
+      <GlobalStyle />
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </AppContainer>
+  );
+};
+export default App;
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,2 @@
+const ContactForm = () => {};
+export default ContactForm;
